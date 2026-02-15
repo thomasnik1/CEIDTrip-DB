4093a50d01fd48e007600dd2a78420fe8cf08149
Thomas Nikolaou <58148283+thomasnik1@users.noreply.github.com>
2026-02-15 02:11:53 +0200

    trip_log table created. to be used later. 90k dummy data. big oof

---
+++ trip_log.sql
@@ -0,0 +1,8 @@
+CREATE TABLE trip_log (
+	past_tr_id INT(11) NOT NULL,
+	past_tr_departure DATETIME NOT NULL,
+	past_tr_return DATETIME NOT NULL,
+	past_tr_dest_count INT,
+	past_tr_participants INT,
+	past_tr_revenue
+);
