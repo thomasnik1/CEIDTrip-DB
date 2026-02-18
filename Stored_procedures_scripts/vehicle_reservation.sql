00052428bfa28941353db7fec1c18343137f1373
Thomas Nikolaou <58148283+thomasnik1@users.noreply.github.com>
2026-02-18 17:32:18 +0200

    kinda finished first procedure

--- Stored_procedures_scripts/vehicle_reservation.sql
+++ Stored_procedures_scripts/vehicle_reservation.sql
@@ -4,24 +4,24 @@ DELIMITER $
 
 CREATE PROCEDURE vehicle_reservation (
 	IN in_tr_id INT(11),
-	IN in_ve_id INT(11)
+	IN in_ve_id INT(11),
+    IN in_ve_mileage FLOAT(10,3)
 )
 
 BEGIN
-    DECLARE vehicle_state ENUM ('AVAILABLE', 'IN USE', 'UNDER MAINTENANCE');
-	DECLARE vehicle_mileage FLOAT(10,3);
-	DECLARE trip_confirmed_reservations INT;
+    DECLARE vehicle_mileage FLOAT(10,3);
+    DECLARE vehicle_state ENUM ('AVAILABLE', 'IN USE', 'UNDER MAINTENANCE');	DECLARE trip_confirmed_reservations INT;
 	DECLARE vehicle_seats INT;
     DECLARE vehicle_driver CHAR(10);
     DECLARE driver_licence ENUM('A', 'B', 'C', 'D');
     DECLARE vehicle_type ENUM('CAR', 'VAN', 'MINIBUS', 'BUS');
-	
+
 	SELECT count(*) INTO trip_confirmed_reservations
 	FROM trip 
 	INNER JOIN reservation ON in_tr_id = reservation.res_tr_id
 	WHERE tr_id = in_tr_id AND res_status = 'CONFIRMED';
 	
-	SELECT ve_seats, ve_state, ve_type, vehicle.ve_mileage INTO vehicle_seats, vehicle_state, vehicle_type, vehicle_mileage
+	SELECT ve_seats, ve_state, ve_type, ve_mileage INTO vehicle_seats, vehicle_state, vehicle_type, vehicle_mileage
 	FROM vehicle
 	WHERE ve_id = in_ve_id;
 
@@ -33,6 +33,9 @@ BEGIN
 	IF vehicle_seats < trip_confirmed_reservations THEN
 	SIGNAL SQLSTATE VALUE '45000'
 	SET MESSAGE_TEXT = 'Not enough seats in this vehicle';
+    ELSEIF in_ve_mileage < vehicle_mileage THEN
+	SIGNAL SQLSTATE VALUE '45000'
+	SET MESSAGE_TEXT = 'Vehicle can not have less mileage than it already has';
 	ELSEIF vehicle_state = 'UNDER MAINTENANCE' OR 'IN USE' THEN
     SIGNAL SQLSTATE VALUE '45000'
 	SET MESSAGE_TEXT = 'Vehicle is unavailable.';
@@ -40,12 +43,14 @@ BEGIN
     SIGNAL SQLSTATE VALUE '45000'
     SET MESSAGE_TEXT = 'Driver is not licensed to drive this vehicle';
     ELSE
-	SELECT CONCAT(vehicle_mileage);
+	UPDATE vehicle
+	SET ve_state = 'IN USE', ve_mileage = in_ve_mileage
+	WHERE ve_id = in_ve_id;
 	END IF;
 
 END $
 
 DELIMITER ;
 
-CALL vehicle_reservation(6, 5);
+CALL vehicle_reservation(6, 5, 300.3);
 
